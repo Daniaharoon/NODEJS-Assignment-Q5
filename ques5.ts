58376399f86a53042a8a34357648da51c4d5aaac
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:14:07 +0500

    Add files via upload

---
+++ ques5.ts
@@ -0,0 +1,5 @@
+//5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person : string = "Allama Iqbal"
+let message : string = "Take it from me that all knowledge is useless until it is connected with your life, because the purpose of knowledge is nothing but to show you the splendors of ...";
+console.log(`${famous_person} Once said, ${message}`);
